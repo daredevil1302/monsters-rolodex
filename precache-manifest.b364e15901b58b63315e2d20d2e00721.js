4b45f9fff42b0fef874636291a91cbe75470aae6
daredevil1302 <markperanovic@gmail.com>
2020-05-21 13:26:25 +0200

    Updates

---
+++ precache-manifest.b364e15901b58b63315e2d20d2e00721.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3767200ebe09af525ee0351d6f394bb3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f963d1d4ab2f15c1797b",
+    "url": "/monsters-rolodex/static/css/main.f8f65c40.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f963d1d4ab2f15c1797b",
+    "url": "/monsters-rolodex/static/js/main.102f32b7.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
